c0880979efc508f79ebf48616f7377f4218d3a9b
kamal <mnkamal95@gmail.com>
2022-02-14 23:23:51 +0800

    Link up components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { ClipGallery } from "./ClipGallery";
+import { ReelCreator } from "./ReelCreator";
+import { ReelEditor } from "./ReelEditor";
+import { useStore } from "./store";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 function App() {
-  return <div className="bg-red-500">hello world</div>;
+  const reel = useStore((state) => state.reelInEdit);
+
+  return (
+    <>
+      <ToastContainer />
+      <section className="bg-red-500 h-80 pt-5">
+        {reel ? <ReelEditor /> : <ReelCreator />}
+      </section>
+      <ClipGallery />
+    </>
+  );
 }
 
 export default App;
